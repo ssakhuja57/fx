2324eddfb4aa96def2385f5c6697005418f4cd66
dave m <david.muchene@gmail.com>
2015-05-12 21:55:36 -0700

    Trade Manager

--- src/com/peebeekay/fx/simulation/trader/TradeManager.java
+++ src/com/peebeekay/fx/simulation/trader/TradeManager.java
@@ -4,9 +4,12 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
 
+import com.peebeekay.fx.simulation.data.IDataSubscriber;
+import com.peebeekay.fx.simulation.data.Price;
 import com.peebeekay.fx.simulation.monitor.ICloseMonitor;
 import com.peebeekay.fx.simulation.monitor.IOpenMonitor;
 import com.peebeekay.fx.simulation.monitors.ATradeMonitor;
+import com.peebeekay.fx.simulation.monitors.cancel.ACancelTradeMonitor;
 import com.peebeekay.fx.simulation.monitors.close.ACloseTradeMonitor;
 import com.peebeekay.fx.simulation.monitors.open.AOpenTradeMonitor;
 import com.peebeekay.fx.simulation.trades.Trade;
@@ -26,27 +29,28 @@ public class TradeManager {
 	}
 	
 	public void updateTrade(Trade trade,AOpenTradeMonitor monitor){
-		if(monitors.containsKey(trade)){
+		monitors.get(trade).openMonitors.add(monitor);
 			
-		}
-		
 	}
 	
 	public void updateTrade(Trade trade, ACloseTradeMonitor monitor){
-		
+		monitors.get(trade).closeMonitors.add(monitor);
 	}
+	
+	
 	
 	class MonitorPair{
 		public Queue<AOpenTradeMonitor> openMonitors;
 		public Queue<ACloseTradeMonitor> closeMonitors;
+		public Queue<ACancelTradeMonitor> cancelTradeMonitors;
 		public MonitorPair()
 		{
 			openMonitors = new LinkedList<AOpenTradeMonitor>();
 			closeMonitors = new LinkedList<ACloseTradeMonitor>();
+			cancelTradeMonitors = new LinkedList<ACancelTradeMonitor>();
+			
 		}
-		
-	}
-	
-	
+	}	
+
 
 }
