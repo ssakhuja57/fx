7cc4eae8566e31d6f5bcc4451a1eed941082a0e6
ssakhuja <shubham.sakhuja@mineraltree.com>
2015-08-25 12:58:06 -0400

    changing runner

--- src/com/peebeekay/fx/strategies/rsi/Runner.java
+++ src/com/peebeekay/fx/strategies/rsi/Runner.java
@@ -20,11 +20,15 @@ public class Runner {
 	
 	static void standard(){
 		
-		Pair pair = Pair.EURUSD;
 		Interval interval = Interval.M30;
 		
 		List<Pair> pairs = new ArrayList<Pair>();
-		pairs.add(pair);
+		pairs.add(Pair.EURUSD);
+		pairs.add(Pair.USDJPY);
+		pairs.add(Pair.GBPUSD);
+		pairs.add(Pair.AUDUSD);
+		pairs.add(Pair.USDCAD);
+		pairs.add(Pair.NZDUSD);
 		
 		List<Interval> intervals = new ArrayList<Interval>();
 		intervals.add(interval);
@@ -34,7 +38,13 @@ public class Runner {
 		ATickDataDistributor tDD = new FxcmTickDataDistributor(fx);
 		OhlcDataDistributor ohlcDD = new OhlcDataDistributor(fx, pairs, intervals);
 		
-		RsiTrader t1 = new RsiTrader("test1", fx, fx, fx, tDD, ohlcDD, interval, pair, 9999, 1);
+		RsiTrader t1 = new RsiTrader("test1", fx, fx, fx, tDD, ohlcDD, interval, pairs.get(0), 9999, 1);
+		RsiTrader t2 = new RsiTrader("test2", fx, fx, fx, tDD, ohlcDD, interval, pairs.get(1), 9999, 1);
+		RsiTrader t3 = new RsiTrader("test3", fx, fx, fx, tDD, ohlcDD, interval, pairs.get(2), 9999, 1);
+		RsiTrader t4 = new RsiTrader("test4", fx, fx, fx, tDD, ohlcDD, interval, pairs.get(3), 9999, 1);
+		RsiTrader t5 = new RsiTrader("test5", fx, fx, fx, tDD, ohlcDD, interval, pairs.get(4), 9999, 1);
+		RsiTrader t6 = new RsiTrader("test6", fx, fx, fx, tDD, ohlcDD, interval, pairs.get(5), 9999, 1);
+		
 
 	}
 	
